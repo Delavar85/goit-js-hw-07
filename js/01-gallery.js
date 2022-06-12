4a3b08738bc98aea3f74354daa23ec68c45e4c6e
Kostya <kosmat3101@gmail.com>
2022-06-12 16:58:32 +0300

    complite

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryBlock = document.querySelector('.gallery');
 const marcupGallery = createMarcupGallery(galleryItems);
-console.log(galleryItems);
 function createMarcupGallery(items) {
     return items.map(({preview,original,description}) => {
         return `<div class="gallery__item">
@@ -18,3 +17,16 @@ function createMarcupGallery(items) {
     }).join('')
 };
 galleryBlock.innerHTML = marcupGallery;
+
+function onClickImage(evt) {
+    evt.preventDefault();
+    const ectivElem = evt.target;
+    if (ectivElem.classList.contains('gallery')) return;
+    const imageSrc = ectivElem.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${imageSrc}" width="800" height="600">
+`);
+
+    instance.show();
+}
+galleryBlock.addEventListener('click', onClickImage)
